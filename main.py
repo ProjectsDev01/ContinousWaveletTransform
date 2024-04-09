ab353cada49de215c5dae8c334321cf9ee279470
Golis <bartosz.golis@gmail.com>
2024-04-09 22:32:04 +0200

    Added opening folder with images

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import pywt
+import cv2
+import os
+from tkinter import filedialog
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def wavelet_transform(image_path, wavelet='haar', level=1):
+    # Wczytanie obrazu
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
+    # Zastosowanie falkowej transformacji dyskretnej
+    coeffs = pywt.wavedec2(img, wavelet, level=level)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Wizualizacja współczynników
+    fig, axs = plt.subplots(1, level+2, figsize=(12, 6))
 
+    # Wyświetlenie oryginalnego obrazu
+    axs[0].imshow(img, cmap='viridis')
+    axs[0].set_title('Original Image')
+    axs[0].axis('off')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    for i in range(1, level+2):
+        # Zastosowanie odwrotnej transformacji falkowej
+        reconstructed_img = pywt.waverec2(coeffs[:i+1], wavelet)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Wyświetlenie zrekonstruowanego obrazu na danej poziomie
+        axs[i].imshow(reconstructed_img.astype(np.uint8), cmap='gray')
+        axs[i].set_title(f'Level {i}')
+        axs[i].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+def select_image():
+    # Tworzenie interfejsu użytkownika Tkinter
+    root = tk.Tk()
+    root.withdraw()  # Ukrycie głównego okna
+
+    # Wybieranie pliku obrazu
+    file_path = filedialog.askopenfilename(initialdir=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images'),
+                                           title="Select an Image",
+                                           filetypes=[("Image Files", "*.jpg; *.jpeg; *.png; *.bmp")])
+
+    if file_path:
+        # Wywołanie funkcji do transformacji falkowej dla wybranego obrazu
+        wavelet_transform(file_path, wavelet='haar', level=3)
+
+# Wywołanie funkcji do wyboru obrazu
+select_image()
